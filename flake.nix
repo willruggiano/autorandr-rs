6881039766b7dce241dd3f0f98f8f34f54ce2456
Will Ruggiano <wmruggiano@gmail.com>
2021-11-03 18:22:17 +0000

    Add defaultPackage to flake.nix for easier consumption

--- flake.nix
+++ flake.nix
@@ -3,19 +3,30 @@
   inputs = {
       nixpkgs.url = "github:nixos/nixpkgs/master";
       flake-utils.url = "github:numtide/flake-utils/master";
+      naersk.url = "github:nmattia/naersk";
       rust-overlay.url = "github:oxalica/rust-overlay/master";
       rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
       rust-overlay.inputs.flake-utils.follows = "flake-utils";
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ rust-overlay.overlay ];
         pkgs = import nixpkgs { inherit system overlays; };
+        naersk-lib = naersk.lib.${system};
         my-rust = pkgs.rust-bin.nightly.latest.default.override {
           extensions = [ "rust-src" "llvm-tools-preview" ];
         };
       in {
+        defaultPackage = naersk-lib.buildPackage rec {
+          pname = "monitor-layout";
+          root = ./.;
+          nativeBuildInputs = with pkgs; [ scdoc installShellFiles ];
+          preFixup = ''
+            installManPage man/${pname}.*.scd
+          '';
+        };
+
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             linuxPackages.perf
